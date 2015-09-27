93b072bbb9570ed3e2d74ac10d0f2601f4b1dd5e
Da Meng <dameng@utexas.edu>
2015-09-26 22:31:11 -0500

    bugs fixed

--- mymalloc.c
+++ mymalloc.c
@@ -24,9 +24,10 @@ void my_malloc_init(size_t size)
    block->next = NULL;
 }
 
-
+/*
+ * slice current block into current using block and free block
+ */
 void slice(struct Block *cur, size_t size) {
-   printf("%s\n", "slice");
    struct Block *free_b = (struct Block*)((char*)cur + size);
    free_b->size = (cur->size) - size;
    free_b->is_free = 1;  
@@ -36,6 +37,9 @@ void slice(struct Block *cur, size_t size) {
    cur->next = free_b;
 }
 
+/*
+ * FIFO: iteratively look for the first block that is big enough
+ */
 void *my_malloc(size_t size)
 {
    struct Block *cur;
@@ -44,16 +48,15 @@ void *my_malloc(size_t size)
    cur = block;
    while ((cur->size < size || cur->is_free == 0) && (cur->next != NULL)) {
       cur = cur->next;
-      printf("Address of cur is %" PRIu64 "\n",my_address(cur));
    }
 
    if (cur->size == size) {
       cur->is_free = 0;
-      result = (void*) (++cur);
+      result = (void*) (cur);
       return result;
    } else if (cur->size > (size)) {
       slice(cur, size);
-      result = (void*)(++cur);
+      result = (void*)(cur);
       return result;
    } else {
       printf("%s\n", "no memory");
@@ -61,25 +64,29 @@ void *my_malloc(size_t size)
    }
 }
 
-
+/*
+ * iterate all blocks and join those that are adjacently free
+ */
 void join() {
-   printf("%s\n", "join");
    struct Block *cur;
    cur = block;
    while(cur->next != NULL) {
       if (cur->is_free && cur->next->is_free) {
-         printf("Address of cur is %" PRIu64 ", size is %i\n",my_address(cur), cur->size);
          cur->size += (cur->next->size);
          cur->next = cur->next->next;
+      } else {
+         cur = cur->next;
       }
-      cur = cur->next;
+      
    }
 }
 
+/*
+ * free current and join
+ */
 void my_free(void *ptr)
 {
    struct Block *cur = ptr;
-   --cur;
    cur->is_free = 1;
    join();
 }
@@ -112,22 +119,13 @@ static void draw_box(FILE *stream, int size, int empty, int last)
 
 void my_dump_mem(FILE *stream)
 {
-   /* TODO: implement */
    struct Block *cur;
    cur = block;
    while(cur->next != NULL) {
-      //printf("Address is %" PRIu64 "\n", my_address(cur));
       draw_box(stream, (int)(cur->size/MiB), cur->is_free, 0);
       cur = cur->next;      
    }
    draw_box(stream, cur->size / MiB, cur->is_free, 1);
-
-   /* the following is an example */
-   /* of a heap with four */
-   /* blocks of sizes  */
-   /* 1, 4, 1, and 5  */
-   /* and the third block unallocated */
-   /* (empty) */
 
 }
 
